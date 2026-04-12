cdfe79c9010a2db85741a26600bf7c8d9e4531db
Srajan Kushwaha <srajank2003@gmail.com>
2026-04-12 13:24:19 +0530

    GEN-AI: Prompts:: 2. System + User Prompt

--- Generative-AI_By_sheryians/chatmodels/chatbot.py
+++ Generative-AI_By_sheryians/chatmodels/chatbot.py
@@ -3,20 +3,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
 
 model = ChatMistralAI(model = "mistral-small-2506",temperature=0.9)
 
-messages = []
+messages = [
+    SystemMessage(content='you are a funny AI agent')
+]
 
 print("--------------- Welcome type 0 to exit the application ---------")
+
 while True:
     prompt = input("You : ")
-
-    messages.append(prompt)
+    messages.append(HumanMessage(content=prompt))
 
     if prompt == '0':
         break
 
     response = model.invoke(messages)
-    messages.append(response.content)
+    messages.append(AIMessage(content=response.content))
     print("Bot : ", response.content)
